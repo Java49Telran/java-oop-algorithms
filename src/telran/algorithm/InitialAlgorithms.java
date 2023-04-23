4d57ab12a4ee2a7ea7335ebe18318d2d8172c20a
Yuragranovsky <yuragranovsky@gmail.com>
2023-04-23 21:20:29 +0300

    CW #7 Algorithm complexities

--- src/telran/algorithm/InitialAlgorithms.java
+++ src/telran/algorithm/InitialAlgorithms.java
@@ -2,6 +2,31 @@ package telran.algorithm;
 
 public class InitialAlgorithms {
       public static void sortShortPositive(short [] array) {
-    	  //array - array of the positive short numbers
+    	  int [] helper = new int[Short.MAX_VALUE];
+    	  //helper[index] => count of occurrences for number index in array
+    	  //helper[1000] = 2 => number 1000 occurs twice in the source array
+    	  //helper[2] = 0;
+    	  for(int i = 0; i < array.length; i++) {
+    		  helper[array[i]]++; 
+    	  }
+    	  int ind = 0;
+    	  for(int i = 0; i < helper.length; i++) {
+    		  for(int j = 0; j < helper[i]; j++) {
+    			  array[ind++] = (short) i;
+    		  }
+    	  }
+
+      }
+      public static boolean isSum2(short[] array, short sum) {
+    	  //returns true if there are two numbers in the given array,
+    	  // sum of which equals the given sum value
+    	  //otherwise false
+    	  //TODO
+    	  return false;
+      }
+      public static short getMaxPositiveWithNegativeReflect(short[] array) {
+    	  //returns maximal positive number, for which there is the negative image
+    	  //If there are not such numbers at all the method returns -1
+    	  return -1;
       }
 }
