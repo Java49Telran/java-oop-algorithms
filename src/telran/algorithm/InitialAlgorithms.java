ad714031d529dbbe7cff3aa1e22adb07e4e1daa1
Yuragranovsky <yuragranovsky@gmail.com>
2023-04-23 20:02:25 +0300

    Intermediate commit

---
+++ src/telran/algorithm/InitialAlgorithms.java
@@ -0,0 +1,7 @@
+package telran.algorithm;
+
+public class InitialAlgorithms {
+      public static void sortShortPositive(short [] array) {
+    	  //array - array of the positive short numbers
+      }
+}
