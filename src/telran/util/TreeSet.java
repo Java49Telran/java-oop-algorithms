a1584e0577cd6e5e426b43c7f8254cecd5183df1
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-28 21:20:40 +0300

    CW #16 TreeSet add / iterator

--- src/telran/util/TreeSet.java
+++ src/telran/util/TreeSet.java
@@ -2,6 +2,7 @@ package telran.util;
 
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class TreeSet<T> implements Set<T> {
 	private static class Node<T> {
@@ -9,34 +10,116 @@ public class TreeSet<T> implements Set<T> {
 		Node<T> parent;
 		Node<T> left;
 		Node<T> right;
+		Node(T obj) {
+			this.obj = obj;
+		}
 		
 	}
 	private Node<T> root;
 	private Comparator<T> comp;
 	private int size;
 	private class TreeSetIterator implements Iterator<T> {
-
+		Node<T> current;
+		Node<T> prev;
+		boolean flNext = false;
+		TreeSetIterator() {
+			current = root == null ? null : getLeast(root);
+		}
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
+			
+			return current != null;
 		}
 
 		@Override
 		public T next() {
-			// TODO Auto-generated method stub
-			return null;
+			if(!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			T res = current.obj;
+			prev = current;
+			current = getCurrent(current);
+			flNext = true;
+			return res;
 		}
 		@Override
 		public void remove() {
-			//TODO
+			if(!flNext) {
+				throw new IllegalStateException();
+			}
+			removeNode(prev);
+			flNext = false;
 		}
 	}
 	
 	@Override
 	public boolean add(T obj) {
-		// TODO Auto-generated method stub
-		return false;
+		Node<T> node = new Node<>(obj);
+		boolean res = false;
+		if (size == 0) {
+			root = node;
+			res = true;
+		} else {
+			Node<T> parent = getParent(obj);
+			if (parent != null) {
+				res = true;
+				node.parent = parent;
+				if(comp.compare(obj, parent.obj) > 0) {
+					parent.right = node;
+				} else {
+					parent.left = node;
+				}
+			}
+		}
+		if (res) {
+			size++;
+		}
+		return res;
+	}
+	private Node<T> getCurrent(Node<T> current) {
+		
+		return current.right != null ? getLeast(current.right) :
+			getGreaterParent(current);
+	}
+	private Node<T> getGreaterParent(Node<T> current) {
+		while(current.parent != null && current == current.parent.right) {
+			current = current.parent;
+		}
+		return current.parent;
+	}
+	private Node<T> getLeast(Node<T> node) {
+		Node<T> current = node;
+		while(current.left != null) {
+			current = current.left;
+		}
+		return current;
+	}
+	private Node<T> getNodeParent(T obj) {
+		Node<T> current = root;
+		Node<T> parent = null;
+		int compRes;
+		while(current != null && (compRes = comp.compare(obj, current.obj)) != 0) {
+			parent = current;
+			current = compRes > 0 ? current.right : current.left;
+		}
+		return current == null ? parent : current;
+	}
+	private Node<T> getNode(T obj) {
+		Node<T> node = getNodeParent(obj);
+		Node<T> res = null;
+		if (node != null && comp.compare(obj, node.obj) == 0) {
+			res = node;
+		}
+		return res;
+		
+	}
+	private Node<T> getParent(T obj) {
+		Node<T> node = getNodeParent(obj);
+		Node<T> res = null;
+		if (node != null && comp.compare(obj, node.obj) != 0) {
+			res = node;
+		}
+		return res;
 	}
 
 	@Override
@@ -47,14 +130,24 @@ public class TreeSet<T> implements Set<T> {
 
 	@Override
 	public boolean remove(T pattern) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+		Node<T> node = getNode(pattern);
+		if (node != null) {
+			removeNode(node);
+		}
+		
+		return res;
 	}
 
+	private void removeNode(Node<T> node) {
+		// TODO Auto-generated method stub
+		size--;
+		
+	}
 	@Override
 	public boolean contains(T pattern) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return getNode(pattern) != null;
 	}
 
 	@Override
